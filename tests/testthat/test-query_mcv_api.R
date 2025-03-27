e64a29705200f447ed07616c10a76f48c36fdcc6
PaulMelloy <paul@melloy.com.au>
2025-03-27 15:59:06 +1000

    init mcv api

---
+++ tests/testthat/test-query_mcv_api.R
@@ -0,0 +1,12 @@
+
+.station_code = NULL
+.longitude = 151
+.latitude = -27
+.start_date = "2024-06-01"
+.end_date = "2024-12-01"
+.values = "precip"
+.format = "csv"
+.radius = NULL
+.api_key = "xxxxxxxxxxxxxxxxxxxx"
+.dataset = "observations"
+
